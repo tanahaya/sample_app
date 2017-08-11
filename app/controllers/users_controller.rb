436dbdcfba5226c6c352bab8d93b7a268d89d56b
tanahaya <tanahaya00623@gmail.com>
2017-08-11 05:22:23 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,25 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)   # 実装は終わっていないことに注意!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      # 保存の成功をここで扱う。
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
