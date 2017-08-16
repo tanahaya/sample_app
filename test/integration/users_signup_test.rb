e39bd9b925cafc08d9a1594332378de799f11604
tanahaya <tanahaya00623@gmail.com>
2017-08-16 04:37:56 +0000

    Add password reset

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,11 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-
   def setup
     ActionMailer::Base.deliveries.clear
   end
-  
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -16,9 +15,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
-  
-   test "valid signup information with account activation" do
+    
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
@@ -45,5 +46,4 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert is_logged_in?
   end
-  
 end
